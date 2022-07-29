6ee9c25b56b8d16e7f0686cbc5a2d19bb72b63b8
antonio.borja <antonio.borja@optimissa.com>
2022-07-29 11:14:37 +0200

    ﻿Add IstackingDAO class

--- src/main/java/com/cryptonita/app/data/daos/IStackingDAO.java
+++ src/main/java/com/cryptonita/app/data/daos/IStackingDAO.java
@@ -5,11 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface IStackingDAO extends JpaRepository<StackingModel,Long> {
-
-    List<StackingModel> findAllByUserId(long id);
+public interface IStackingDAO extends JpaRepository <StackingModel,Long> {
 
     StackingModel findByUserId(long id);
 
+    List<StackingModel> findAllByUserId(long id);
 
 }
