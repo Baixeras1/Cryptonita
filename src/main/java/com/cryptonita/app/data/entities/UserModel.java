2a7dcb9f364f550da8a918725bbdc9d3cdb8c01d
sergio.bernal <sergio.bernal@optimissa.com>
2022-07-29 08:52:31 +0200

    Resolved conflicts

--- src/main/java/com/cryptonita/app/data/entities/UserModel.java
+++ src/main/java/com/cryptonita/app/data/entities/UserModel.java
@@ -1,11 +1,41 @@
 package com.cryptonita.app.data.entities;
 
+import com.cryptonita.app.data.entities.enums.UserRole;
+import lombok.*;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
+@Table(name = "USERS")
 public class UserModel {
 
     @Id
     private long id;
+
+    @Column(unique = true)
+    private String mail;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private UserRole role;
+
+    @Builder
+    public UserModel(String mail, String username, String password, UserRole role) {
+        this.mail = mail;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
 }
