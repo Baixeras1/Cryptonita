91cc0ebfc426b82824fbe25a7357cbdd7eaf05e5
OPTIMISSA\carlos.cueva <Carlos>
2022-08-08 09:53:13 +0200

    .

--- src/main/java/com/cryptonita/app/exceptions/data/StackingNotFoundException.java
+++ src/main/java/com/cryptonita/app/exceptions/data/StackingNotFoundException.java
@@ -2,24 +2,24 @@ package com.cryptonita.app.exceptions.data;
 
 import com.cryptonita.app.exceptions.LogicError;
 
-public class StakingNotFoundException extends LogicError {
+public class StackingNotFoundException extends LogicError {
 
-    public StakingNotFoundException() {
+    public StackingNotFoundException() {
     }
 
-    public StakingNotFoundException(String message) {
+    public StackingNotFoundException(String message) {
         super(message);
     }
 
-    public StakingNotFoundException(String message, Throwable cause) {
+    public StackingNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public StakingNotFoundException(Throwable cause) {
+    public StackingNotFoundException(Throwable cause) {
         super(cause);
     }
 
-    public StakingNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public StackingNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
