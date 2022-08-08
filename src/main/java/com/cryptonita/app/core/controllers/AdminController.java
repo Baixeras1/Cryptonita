a52babae9feeb31b2e62ea312f183a8899d49611
antonio.borja <antonio.borja@optimissa.com>
2022-08-08 08:57:09 +0200

    Implement AdminController,IadminService & AdminServiceImpl

--- src/main/java/com/cryptonita/app/core/controllers/AdminController.java
+++ src/main/java/com/cryptonita/app/core/controllers/AdminController.java
@@ -1,2 +1,41 @@
-package com.cryptonita.app.core.controllers;public class AdminController {
+package com.cryptonita.app.core.controllers;
+
+import com.cryptonita.app.core.controllers.services.IAdminService;
+import com.cryptonita.app.dto.data.response.BannedUserResponseDTO;
+import com.cryptonita.app.dto.data.response.CoinResponseDTO;
+import com.cryptonita.app.dto.data.response.UserResponseDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/Admin")
+public class AdminController {
+
+    private final IAdminService adminService;
+
+    @PostMapping("/assets/create")
+    public CoinResponseDTO createCoin(String name,String symbol,int rank){
+        return adminService.createCoin(name,symbol,rank);
+    }
+
+    @DeleteMapping("/assets/delete")
+    public CoinResponseDTO deleteCoin(String name){
+        return adminService.deleteCoin(name);
+    }
+
+    @PostMapping("/users/ban")
+    public BannedUserResponseDTO banUser (String mail){
+        return adminService.banUser(mail);
+    }
+
+    @PostMapping("/users/unBan")
+    public BannedUserResponseDTO unBanUser(String mail){
+        return adminService.unBanUser(mail);
+    }
+
+    @GetMapping("/users/get{id}")
+    public UserResponseDTO getUserById(long id){
+        return adminService.getUserById(id);
+    }
 }
--- src/main/java/com/cryptonita/app/core/controllers/services/impl/AdminServiceImpl.java
+++ src/main/java/com/cryptonita/app/core/controllers/services/impl/AdminServiceImpl.java
@@ -1,2 +1,48 @@
-package com.cryptonita.app.core.controllers.services.impl;public class AdminServiceImpl {
+package com.cryptonita.app.core.controllers.services.impl;
+
+
+import com.cryptonita.app.core.controllers.services.IAdminService;
+import com.cryptonita.app.data.providers.ICoinProvider;
+import com.cryptonita.app.data.providers.IUserProvider;
+import com.cryptonita.app.dto.data.response.BannedUserResponseDTO;
+import com.cryptonita.app.dto.data.response.CoinResponseDTO;
+import com.cryptonita.app.dto.data.response.UserResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class AdminServiceImpl implements IAdminService {
+
+    private final ICoinProvider coinProvider;
+    private final IUserProvider userProvider;
+
+
+    @Override
+    public CoinResponseDTO createCoin(String name,String symbol,int rank) {
+        //log.info(); //TODO
+        return coinProvider.createCoin(name,symbol,rank);
+    }
+
+    @Override
+    public CoinResponseDTO deleteCoin(String name) {
+        return coinProvider.deleteByName(name);
+    }
+
+    @Override
+    public BannedUserResponseDTO banUser(String mail) {
+        return userProvider.banUser(mail);
+    }
+
+    @Override
+    public BannedUserResponseDTO unBanUser(String mail) {
+        return userProvider.unBanUser(mail);
+    }
+
+    @Override
+    public UserResponseDTO getUserById(long id) {
+        return userProvider.getById(id);
+    }
 }
