d318d35ad0c73db29f4765283b886082b2c5b368
OPTIMISSA\carlos.cueva <Carlos>
2022-08-10 16:06:11 +0200

    [#27332]Autentification

--- src/main/java/com/cryptonita/app/data/entities/UserModel.java
+++ src/main/java/com/cryptonita/app/data/entities/UserModel.java
@@ -3,8 +3,14 @@ package com.cryptonita.app.data.entities;
 import com.cryptonita.app.data.entities.enums.UserRole;
 import com.cryptonita.app.data.entities.enums.UserType;
 import lombok.*;
+import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,18 +21,23 @@ import java.util.List;
 @EqualsAndHashCode(of = {"id", "username"})
 @ToString(exclude = {"account", "favourites"})
 @Table(name = "USERS")
+@Valid
 public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
+    @NotEmpty(message = "El campo email no puede estar vacio")
+    @Email
     @Column(unique = true)
     private String mail;
 
+    @NotEmpty(message = "El campo nombre de ususario no puede ir vacio")
     @Column(unique = true)
     private String username;
 
+    @Pattern(regexp = "^((?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])){4,12}$",
+            message = "password must contain atleast 1 uppercase, 1 lowercase, 1 special character and 1 digit ")
     private String password;
 
     @Enumerated(EnumType.STRING)
