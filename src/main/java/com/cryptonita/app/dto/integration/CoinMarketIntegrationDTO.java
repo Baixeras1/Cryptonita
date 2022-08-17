3edbbaf55b4f76fd35bcbe0d54c17b02a9543cda
OPTIMISSA\carlos.cueva <Carlos>
2022-08-17 10:28:00 +0200

    [#27407] Create CoinMarketIntegrationDTO

---
+++ src/main/java/com/cryptonita/app/dto/integration/CoinMarketIntegrationDTO.java
@@ -0,0 +1,41 @@
+package com.cryptonita.app.dto.integration;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Builder
+@ToString
+public class CoinMarketIntegrationDTO {
+
+    private String id;
+    private String symbol;
+    private String name;
+    private String image;
+    private double current_price;
+    private double market_cap;
+    private int markert_cap_rank;
+    private double fully_diluted_valuation;
+    private double total_volume;
+    private double high_24h;
+    private double low_24;
+    private double price_change_24h;
+    private double price_change_percentage_24h;
+    private double market_cap_change_24h;
+    private double market_cap_change_percentage_24h;
+    private double circulating_supply;
+    private double total_supply;
+    private double max_supply;
+    private double ath;
+    private double ath_change_percentage;
+    private String ath_date;
+    private double atl;
+    private double atl_change_percentage;
+    private String atl_date;
+    private String roi;
+    private String last_updated;
+
+
+}
