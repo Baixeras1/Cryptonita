f546a4e86e786f38eb8be4154909b0caa7ef8d59
antonio.borja <antonio.borja@optimissa.com>
2022-08-22 10:46:31 +0200

    [#27496] Implementing History Excel Export v.0.1

--- src/main/java/com/cryptonita/app/core/controllers/HistoryController.java
+++ src/main/java/com/cryptonita/app/core/controllers/HistoryController.java
@@ -1,14 +1,22 @@
 package com.cryptonita.app.core.controllers;
 
 import com.cryptonita.app.core.controllers.services.IHistoryService;
+import com.cryptonita.app.core.controllers.services.excel.ExcelGenerator;
 import com.cryptonita.app.core.controllers.utils.RestResponse;
+import com.cryptonita.app.dto.data.response.HistoryResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -22,12 +30,31 @@ public class HistoryController {
     @GetMapping("/history")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Get historical operations of the current user at a given date range")
-    public RestResponse getHistoryByUserName(LocalDate start, LocalDate end){
-        return RestResponse.encapsulate(historyService.getAllRegisterUser(start,end));
+    public RestResponse getHistoryByUserName(LocalDate start, LocalDate end) {
+        return RestResponse.encapsulate(historyService.getAllRegisterUser(start, end));
     }
 
-    @GetMapping("/dowload")
+    @GetMapping("/download-to-excel")
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Get historical operations of the current user as a pdf at a given date range (BETA)")
-    public RestResponse dowloadHistory(LocalDate start,LocalDate end){return null;}
+    @Operation(summary = "Get historical operations of the current user as a excel at a given date range (BETA)")
+    public void downloadHistory(String start, String end, HttpServletResponse response) throws IOException {
+
+        LocalDate localDateStart = LocalDate.parse(start);
+        LocalDate localDateEnd = LocalDate.parse(end);
+
+        response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=history" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        List<HistoryResponseDTO> historyResponseDTOList = historyService.getAllRegisterUser(localDateStart, localDateEnd);
+
+        ExcelGenerator generator = new ExcelGenerator(historyResponseDTOList);
+
+        generator.generateExcelFile(response);
+
+    }
 }
